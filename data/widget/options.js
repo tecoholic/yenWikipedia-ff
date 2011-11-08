efa8dbc63305e56060bc669bab7facc630982d5d
Arunmozhi <aruntheguy@gmail.com>
2011-11-08 14:13:17 +0530

    languages sent as messages

--- data/widget/options.js
+++ data/widget/options.js
@@ -2,13 +2,23 @@ function appendOption(container, language) {
     $(container).append("<input type='radio' name='longradio' id='" + language.code + "' value='" + language.name + "' /> <label for='" + language.code + "' title='" + language.title + "'>" + language.name + "</label>");
 }
 function saveLang(lang, name) {
-        localStorage["language"] = lang;
+      //  localStorage["language"] = lang;
+      self.postMessage(lang);
+      console.log(lang+" sent to addon script")
         $("#notice").clearQueue();
         $("#notice").text("Language changed to " + name + "");
 
         $("#notice").fadeIn().delay(3000).fadeOut();
 
 }
+
+var lang;
+self.on('message', function(data){
+    lang = data;
+    console.log("Recieved initial lang" + data);
+});
+
+
 $(document).ready(function() {
     $.each(primaryIndicLanguages, function(i, v) {
         appendOption("#primaryIndicChoices", v);
@@ -19,8 +29,7 @@ $(document).ready(function() {
     $(".choiceSet").buttonset();
     $("#showOther").button();
     $("#saveOther").button();
-    var lang = localStorage["language"];
-    $(".choiceSet input[type='radio']").change(function() {
+        $(".choiceSet input[type='radio']").change(function() {
         saveLang($(this).attr("id"), $(this).attr("value"));
     });
     $("#otherChoice").val(lang);
